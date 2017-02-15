375d675687763206c7505af47bc860af1b685680
Luis E. Salgado <Luis E. Salgado>
2017-02-14 20:09:33 -0800

    we now have new mock data in db.json

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -1,7 +1,7 @@
 /* This script generates mock data for local development.
-    This way you don't have to point to an actual API,
-        but you can enjoy realistic, but randomized data,
-        and rapid page loads due to local, static data.
+  This way you don't have to point to an actual API,
+  but you can enjoy realistic, but randomized data,
+  and rapid page loads due to local, static data.
 */
 
 /* es-lint-disable no-console */

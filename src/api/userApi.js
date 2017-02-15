030864acda0b5ef562af8f4fa15b0d7afbf6bfc2
Luis E. Salgado <Luis E. Salgado>
2017-02-14 20:49:57 -0800

    added delete function to table

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,25 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers(){
   return get('users');
 }
 
+export function deleteUser(id){
+    return del(`users/${id}`);
+}
+
 function get(url){
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url){
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response){
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import {getUsers} from './api/userApi';
+import'./index.css';
+
+import {getUsers, deleteUser} from './api/userApi';
 
 //Populate table of users via API call.
 getUsers().then(result => {
@@ -15,4 +17,18 @@ getUsers().then(result => {
   });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementByClassName('deleteUser');
+
+    //Must use array.from to create a real array from a DOM collection
+    //getElementByClassname only returns an "array like" object
+    Array.from(deleteLinks, link => {
+      link.onclick = function(event){
+        const element = event.target;
+        event.preventDefault();
+        deleteUser(element.attributes["data-id"].value);
+        const row = element.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+      };
+    });
 });

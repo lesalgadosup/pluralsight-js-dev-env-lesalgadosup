8f767850c001986677a4de2519a5d32bb3728473
Luis E. Salgado <Luis E. Salgado>
2017-02-14 19:39:56 -0800

    mock data generated and html rrenders mock data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import './index.css';
+import {getUsers} from './api/userApi';
 
-import numeral from 'numeral';
+//Populate table of users via API call.
+getUsers().then(result => {
+  let userBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);
+  result.forEach(user => {
+    usersBody+= `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`
+  });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});

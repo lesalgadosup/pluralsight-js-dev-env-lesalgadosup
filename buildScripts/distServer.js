1df403e6f9c7e0353429a6a90f447e5ee8bedaa5
Luis E. Salgado <Luis E. Salgado>
2017-02-16 13:57:42 -0800

    added deployment script to cycle

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -16,15 +16,6 @@ app.get('/',function(req, res){
     res.sendFile(path.join(__dirname,'../src/index.html')); //declared routing
 });
 
-app.get('/users', function(req, res){
-  // Hard coding for simplicy. Pretend this hits a real database
-  res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy", "lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
-});
-
 app.listen(port, function(err){
   if (err){
     console.log(err);
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://warm-badlands-54622.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url){
